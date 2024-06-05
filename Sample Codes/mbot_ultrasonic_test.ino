103f5afbaef9984ed6e79822ec428aee440f548c
sangitsdhar <35770353+sangitsdhar@users.noreply.github.com>
2024-06-05 11:41:21 +0800

    ultrasonic test file

---
+++ Sample Codes/mbot_ultrasonic_test.ino
@@ -0,0 +1,25 @@
+/**
+Functions used from the Makeblock libraries
+Author: Sangit Sasidhar
+**/
+
+#include "MeMCore.h"
+
+#define SERIAL_MONITOR_ACTIVATE 1 /* Set to 1 to use the serial monitor; set to 0 to disable the serial monitor  */
+
+MeUltrasonic ultraSensor(PORT_3); 
+
+void setup()
+{
+  if(SERIAL_MONITOR_ACTIVATE)
+    Serial.begin(9600);
+}
+
+void loop()
+{
+  Serial.print("Distance : ");
+  Serial.print(ultraSensor.distanceCm() );
+  Serial.println(" cm");
+  delay(100);
+}
+
