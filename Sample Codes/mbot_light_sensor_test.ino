eecc59450682b4879e0c4cd9f17015b1dff47d85
sangitsdhar <35770353+sangitsdhar@users.noreply.github.com>
2024-06-05 12:07:47 +0800

    light sensor test file

---
+++ Sample Codes/mbot_light_sensor_test.ino
@@ -0,0 +1,29 @@
+/**
+Functions used from the Makeblock libraries
+Author: Sangit Sasidhar
+**/
+
+#include "MeMCore.h"
+
+#define SERIAL_MONITOR_ACTIVATE 1 /* Set to 1 to use the serial monitor; set to 0 to disable the serial monitor  */
+
+MeLightSensor lightSensor(PORT_6);
+int light_value = 0;      /* a variable for the lightSensor's value */
+
+void setup()
+{
+  if(SERIAL_MONITOR_ACTIVATE)
+    Serial.begin(9600);
+}
+
+void loop()
+{
+  light_value = lightSensor.read();
+
+  // print the results to the serial monitor
+  Serial.print("value = ");
+  Serial.println(light_value);
+  // wait 100 milliseconds before the next loop
+  delay(500);
+
+}
